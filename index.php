5a41a38aa28186cc640207769e3b61933a5292ed
Emy0295 <57665311+Emy0295@users.noreply.github.com>
2020-09-06 11:24:17 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page for TECHMAHINDRA v4.1 FINAL FINAL HURRAY </h1>
+
+<?php
+echo "Hello World! from GIT";
+echo "Hello from version1.1  ";
+echo " Why to Worry ? S2I in openshift";
+?>
+
+</body>
+</html>
